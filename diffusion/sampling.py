6ff049f409ab1bbceff9d687bbb3725099aee839
Jihyeon Je <je@lml-gpu1.dev.bb.schrodinger.com>
2022-07-28 21:29:49 +0000

    remove obsolete conditions

--- diffusion/sampling.py
+++ diffusion/sampling.py
@@ -1,9 +1,7 @@
-import random
 from utils.featurization import featurize_mol, featurize_mol_from_smiles
 from utils.torsion import *
 from diffusion.likelihood import *
 import torch, copy
-from copy import deepcopy
 import numpy as np
 from torch_geometric.data import Dataset
 from torch_geometric.loader import DataLoader
@@ -12,8 +10,6 @@ from rdkit.Chem import AllChem
 from utils.visualise import PDBFile
 
 device = torch.device('cpu')
-still_frames = 10
-
 
 def try_mmff(mol):
     try:
@@ -24,23 +20,15 @@ def try_mmff(mol):
 
 
 def get_seed(smi, seed_confs=None, dataset='drugs'):
-    if seed_confs:
-        if smi not in seed_confs:
-            print("smile not in seeds", smi)
-            return None, None
-        mol = seed_confs[smi][0]
-        data = featurize_mol(mol, dataset)
-
-    else:
-        mol, data = featurize_mol_from_smiles(smi, dataset=dataset)
-        if not mol:
-            return None, None
+    mol, data = featurize_mol_from_smiles(smi, dataset=dataset)
+    if not mol:
+        return None, None
     data.edge_mask, data.mask_rotate = get_transformation_mask(data)
     data.edge_mask = torch.tensor(data.edge_mask)
     return mol, data
 
 
-def embed_seeds(mol, data, n_confs, single_conf=False, smi=None, embed_func=None, seed_confs=None, pdb=None, mmff=False):
+def embed_seeds(mol, data, n_confs, single_conf=False, smi=None, embed_func=None, seed_confs=None):
     if not seed_confs:
         embed_num_confs = n_confs if not single_conf else 1
         try:
@@ -48,94 +36,57 @@ def embed_seeds(mol, data, n_confs, single_conf=False, smi=None, embed_func=None
         except Exception as e:
             print(e.output)
             pass
-        if len(mol.GetConformers()) != embed_num_confs:
+        if len(mol.GetConformers())!=embed_num_confs:
             print(len(mol.GetConformers()), '!=', embed_num_confs)
             return [], None
-        if mmff: try_mmff(mol)
-
-    if pdb: pdb = PDBFile(mol)
     conformers = []
     for i in range(n_confs):
         data_conf = copy.deepcopy(data)
-        if single_conf:
-            seed_mol = copy.deepcopy(mol)
-        elif seed_confs:
-            seed_mol = random.choice(seed_confs[smi])
-        else:
-            seed_mol = copy.deepcopy(mol)
-            [seed_mol.RemoveConformer(j) for j in range(n_confs) if j != i]
-
+        seed_mol = copy.deepcopy(mol)
+        [seed_mol.RemoveConformer(j) for j in range(n_confs) if j != i]
         data_conf.pos = torch.from_numpy(seed_mol.GetConformers()[0].GetPositions()).float()
         data_conf.seed_mol = copy.deepcopy(seed_mol)
-        if pdb:
-            pdb.add(data_conf.pos, part=i, order=0, repeat=still_frames)
-            if seed_confs:
-                pdb.add(data_conf.pos, part=i, order=-2, repeat=still_frames)
-            pdb.add(torch.zeros_like(data_conf.pos), part=i, order=-1)
-
         conformers.append(data_conf)
     if mol.GetNumConformers() > 1:
         [mol.RemoveConformer(j) for j in range(n_confs) if j != 0]
-    return conformers, pdb
+    return conformers
 
 
-def perturb_seeds(data, pdb=None):
-    for i, data_conf in enumerate(data):
+def perturb_seeds(data):
+    for data_conf in data:
         torsion_updates = np.random.uniform(low=-np.pi,high=np.pi, size=data_conf.edge_mask.sum())
         data_conf.pos = modify_conformer(data_conf.pos, data_conf.edge_index.T[data_conf.edge_mask],
                                          data_conf.mask_rotate, torsion_updates)
         data_conf.total_perturb = torsion_updates
-        if pdb:
-            pdb.add(data_conf.pos, part=i, order=1, repeat=still_frames)
     return data
 
 
 def sample(conformers, model, sigma_max=np.pi, sigma_min=0.01 * np.pi, steps=20, batch_size=32,
-           ode=False, likelihood=None, pdb=None):
+           ode=False, likelihood=None):
     conf_dataset = InferenceDataset(conformers)
     loader = DataLoader(conf_dataset, batch_size=batch_size, shuffle=False)
 
     sigma_schedule = 10 ** np.linspace(np.log10(sigma_max), np.log10(sigma_min), steps + 1)[:-1]
     eps = 1 / steps
-
-    for batch_idx, data in enumerate(loader):
-
+    for data in loader:
         dlogp = torch.zeros(data.num_graphs)
         data_gpu = copy.deepcopy(data).to(device)
-        for sigma_idx, sigma in enumerate(sigma_schedule):
-
+        for sigma in sigma_schedule:
             data_gpu.node_sigma = sigma * torch.ones(data.num_nodes, device=device)
             with torch.no_grad():
                 data_gpu = model(data_gpu)
-
             g = sigma * torch.sqrt(torch.tensor(2 * np.log(sigma_max / sigma_min)))
             z = torch.normal(mean=0, std=1, size=data_gpu.edge_pred.shape)
             score = data_gpu.edge_pred.cpu()
-
-            if ode:
-                perturb = 0.5 * g ** 2 * eps * score
-                if likelihood:
-                    div = divergence(model, data, data_gpu, method=likelihood)
-                    dlogp += -0.5 * g ** 2 * eps * div
-            else:
-                perturb = g ** 2 * eps * score + g * np.sqrt(eps) * z
-
+            perturb = g ** 2 * eps * score + g * np.sqrt(eps) * z
             conf_dataset.apply_torsion_and_update_pos(data, perturb.numpy())
             data_gpu.pos = data.pos.to(device)
-
-            if pdb:
-                for conf_idx in range(data.num_graphs):
-                    coords = data.pos[data.ptr[conf_idx]:data.ptr[conf_idx + 1]]
-                    num_frames = still_frames if sigma_idx == steps - 1 else 1
-                    pdb.add(coords, part=batch_size * batch_idx + conf_idx, order=sigma_idx + 2, repeat=num_frames)
-
             for i, d in enumerate(dlogp):
                 conformers[data.idx[i]].dlogp = d.item()
-
     return conformers
 
 
-def pyg_to_mol(mol, data, mmff=True, rmsd=True, copy=True):
+def pyg_to_mol(mol, data, rmsd=True):
     if not mol.GetNumConformers():
         conformer = Chem.Conformer(mol.GetNumAtoms())
         mol.AddConformer(conformer)
@@ -144,11 +95,10 @@ def pyg_to_mol(mol, data, mmff=True, rmsd=True, copy=True):
         coords = coords.double().numpy()
     for i in range(coords.shape[0]):
         mol.GetConformer(0).SetAtomPosition(i, Geometry.Point3D(coords[i, 0], coords[i, 1], coords[i, 2]))
-    if mmff:
-        try:
-            AllChem.MMFFOptimizeMoleculeConfs(mol, mmffVariant='MMFF94s')
-        except Exception as e:
-            pass
+    try:
+        AllChem.MMFFOptimizeMoleculeConfs(mol, mmffVariant='MMFF94s')
+    except Exception as e:
+        pass
     try:
         if rmsd:
             mol.rmsd = AllChem.GetBestRMS(
@@ -159,8 +109,7 @@ def pyg_to_mol(mol, data, mmff=True, rmsd=True, copy=True):
     except:
         pass
     mol.n_rotable_bonds = data.edge_mask.sum()
-    if not copy: return mol
-    return deepcopy(mol)
+    return mol
 
 
 class InferenceDataset(Dataset):
@@ -177,7 +126,6 @@ class InferenceDataset(Dataset):
         return self.data[idx]
 
     def apply_torsion_and_update_pos(self, data, torsion_updates):
-
         pos_new, torsion_updates = perturb_batch(data, torsion_updates, split=True, return_updates=True)
         for i, idx in enumerate(data.idx):
             try:
